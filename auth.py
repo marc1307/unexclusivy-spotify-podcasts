e34fb37f5ac46f8202679a87fd24ad145b0bb7b8
Marc Ernstberger <marc@marc1307.de>
2021-01-29 12:41:14 +0100

    changed authentication to app based

--- auth.py
+++ auth.py
@@ -8,29 +8,78 @@ def buildAuthUrl():
     url = "https://accounts.spotify.com/authorize?response_type=code&client_id={}&redirect_uri={}".format(cfg['api']['client_id'], 'http://localhost')
     return url
 
-def getApiToken(code):
+
+# User Flow... Meh... (not needed, ups)
+def getApiUserToken(code):
     cfg = config.readCfg()
-    code = code.rsplit('=', 1)[1]
+    code = code.rsplit('?code=', 1)[1]
     base = "https://accounts.spotify.com/api/token"
     data = {
         'grant_type': 'authorization_code',
         'code': code,
         'redirect_uri': 'http://localhost'
     }
-    reponse = requests.post(base, data=data, auth=HTTPBasicAuth(cfg['api']['client_id'], cfg['api']['client_secret']))
+    response = requests.post(base, data=data, auth=HTTPBasicAuth(cfg['api']['client_id'], cfg['api']['client_secret']))
     
-    if reponse.status_code == 200:
+    if response.status_code == 200:
         f = open("token.json", "w")
-        f.write(reponse.text)
+        f.write(response.text)
         f.close()
+        print('INFO: token.json has been successfully written')
     else:
-        exit("Meh... {} ({})".format(reponse.status_code, json.loads(reponse.text)["error"]))
+        exit("Meh... {} ({})".format(response.status_code, json.loads(response.text)["error"]))
 
+def refreshApiToken():
+    cfg = config.readCfg()
+    refresh_token = config.getRefreshToken()
+    base = "https://accounts.spotify.com/api/token"
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
+        'client_id': cfg['api']['client_id']
+    }
+    response = requests.post(base, data=data, auth=HTTPBasicAuth(cfg['api']['client_id'], cfg['api']['client_secret']))
+    
+    if response.status_code == 200:
+        f = open("token.json", "w")
+        f.write(response.text)
+        f.close()
+        print('INFO: token.json has been successfully updated')
+        return True
+    else:
+        exit("Meh... {} ({})".format(response.status_code, json.loads(response.text)["error"]))
+
+# App Auth
+def getApiToken():
+    cfg = config.readCfg()
+    base = "https://accounts.spotify.com/api/token"
+    data = {
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(base, data=data, auth=HTTPBasicAuth(cfg['api']['client_id'], cfg['api']['client_secret']))
+    
+    if response.status_code == 200:
+        f = open("token.json", "w")
+        f.write(response.text)
+        f.close()
+        print('INFO: token.json has been successfully written')
+    else:
+        exit("Meh... {} ({})".format(response.status_code, json.loads(response.text)["error"]))
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        authUrl = buildAuthUrl()
-        print("URL: {}".format(authUrl))
+        getApiToken()
+        exit()
+        
+    if len(sys.argv) == 2 and str(sys.argv[1]) == "--user":
+         authUrl = buildAuthUrl()
+         print("Please authorize your client at:\n{}".format(authUrl))
+         exit()
+
+    if len(sys.argv) == 2 and str(sys.argv[1]) == "--reauth":
+        refreshApiToken()
+        exit()
 
     if len(sys.argv) == 2:
-        getApiToken(str(sys.argv[1]))
+        getApiUserToken(str(sys.argv[1]))
+        exit()
